ed94056b688bcc970d9e64c63554bbc275dd8008
mkhan42 <michallekhan9@gmail.com>
2022-06-16 15:08:28 -0400

    heroku testing

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use('/', CommentsRouter)
 app.use("/user", UserRouter);
 
 app.get("/", (req, res) => {
-  res.render("user/login");
+  res.render("user/login.liquid");
 });
 
 const PORT = process.env.PORT
