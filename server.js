c778fe4e6ae3770a3e26cbf4e1f16675f2be7290
mkhan42 <michallekhan9@gmail.com>
2022-06-20 00:28:43 -0400

    fixed something

--- server.js
+++ server.js
@@ -1,91 +1,45 @@
-// require("dotenv").config();
-// const express = require("express");
-// const morgan = require("morgan");
-// const methodOverride = require("method-override");
-// const path = require("path");
-// //const Post = require("./models/posts")
-// const PostRouter = require("./controllers/posts");
-// const CommentsRouter = require("./controllers/comments");
-// const UserRouter = require("./controllers/user");
-// const session = require("express-session");
-// const MongoStore = require("connect-mongo");
-
-// const app = require("liquid-express-views")(express(), {
-//   root: [path.resolve(__dirname, "views/")],
-// });
-
-// app.use(morgan("tiny"));
-// app.use(methodOverride("_method"));
-// app.use(
-//   express.urlencoded({
-//     extended: true,
-//   })
-// );
-
-// app.use(express.static("public"));
-
-// app.use(
-//   session({
-//     secret: process.env.SECRET,
-//     store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }),
-//     saveUninitialized: true,
-//     resave: false,
-//   })
-// );
-
-// app.use("/posts", PostRouter);
-// app.use("/", CommentsRouter);
-// app.use("/user", UserRouter);
-
-// app.get("/", (req, res) => {
-//   res.render("user/login.liquid");
-// });
-
-// const PORT = process.env.PORT;
-// app.listen(PORT, () => console.log(`Now Listening on port ${PORT}`));
-
-require("dotenv").config()
-const express = require("express")
-const morgan = require("morgan")
-const methodOverride = require("method-override")
-const path = require("path")
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const methodOverride = require("method-override");
+const path = require("path");
 //const Post = require("./models/posts")
-const PostRouter = require('./controllers/posts')
-const CommentsRouter = require('./controllers/comments')
+const PostRouter = require("./controllers/posts");
+const CommentsRouter = require("./controllers/comments");
 const UserRouter = require("./controllers/user");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
 
 const app = require("liquid-express-views")(express(), {
   root: [path.resolve(__dirname, "views/")],
-})
+});
 
-app.use(morgan("tiny"))
-app.use(methodOverride("_method"))
+app.use(morgan("tiny"));
+app.use(methodOverride("_method"));
 app.use(
   express.urlencoded({
     extended: true,
   })
-)
+);
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 app.use(
   session({
     secret: process.env.SECRET,
-    store: MongoStore.create({ mongoUrl: process.env.DATABASE_URL }),
+    store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }),
     saveUninitialized: true,
     resave: false,
   })
 );
 
-app.use('/posts', PostRouter)
-app.use('/', CommentsRouter)
+app.use("/posts", PostRouter);
+app.use("/", CommentsRouter);
 app.use("/user", UserRouter);
 
 app.get("/", (req, res) => {
   res.render("user/login.liquid");
 });
 
-const PORT = process.env.PORT
-app.listen(PORT, () => console.log(`Now Listening on port ${PORT}`))
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Now Listening on port ${PORT}`));
